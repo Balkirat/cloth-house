d8fdff1cef60bd3931e022cbe76c94f282c60f17
Balkirat <cheemaballu@gmail.com>
2020-05-10 21:06:02 -0400

    added react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage';
+import React from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import HomePage from "./pages/homepage/homepage";
+
+const HatsPage = () => (
+  <h1>HATS</h1>
+)
 
 function App() {
   return (
-    <div >
-        <HomePage />
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/hats" component={HatsPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Menu-item/menu-item.js
+++ src/components/Menu-item/menu-item.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { withRouter} from 'react-router-dom';
 import "./menu-item.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, match }) => (
+  <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${title}`)}>    
     {/* {{ }}  reason the first pair of bracket is for you to write JS expression in JSX code 
         the second pair of bracket is JS object*/}
+       {console.log(history)}
     <div
       className="background-image"
       style={{ backgroundImage: `url(${imageUrl})` }}
@@ -16,4 +18,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
