d0e1e1c014d27fcbba494b2ccf1f13e92f16074d
Balkirat <cheemaballu@gmail.com>
2020-05-14 22:33:36 -0400

    implemented sign up using firebase

--- src/pages/signInAndSignUp/signInAndSignUp.js
+++ src/pages/signInAndSignUp/signInAndSignUp.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import SignIn from '../../components/SignIn/signIn';
+import SignUp from "../../components/SignUp/signUp";
 import './signInAndSignUp.scss';
 
 const SignInAndSignUpPage = () => (
   <div className='sign-in-and-sign-up'>
     <SignIn />
+    <SignUp />
   </div>
 );
 
