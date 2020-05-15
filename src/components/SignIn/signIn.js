d1cd8df119afcde9a1b0fd87e1b0b00f2440f879
Balkirat <cheemaballu@gmail.com>
2020-05-14 22:43:37 -0400

    finishing sign in

--- src/components/SignIn/signIn.js
+++ src/components/SignIn/signIn.js
@@ -2,7 +2,7 @@ import React from "react";
 import FormInput from "../formInput/formInput";
 import CustomButton from "../CustomButton/customButton";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 import "./signIn.scss";
 
@@ -16,9 +16,19 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async event => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+
+    const { email, password } = this.state;
+
+    try{
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error){
+      console.log(error)
+    }
+
+    
   };
 
   handleChange = (event) => {
